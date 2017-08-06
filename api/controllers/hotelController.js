c5067165311eda9d10185b30ca493ed26e80ad85
Karen Garcia <karengarmed@gmail.com>
2017-08-06 18:16:02 -0300

    Creación de archivo mock de hoteles

--- api/controllers/hotelController.js
+++ api/controllers/hotelController.js
@@ -1,25 +1,6 @@
 exports.getAll                  = function(req, res) {
 
-  var hotels                    = { 
-    "hotels" : [
-
-      {
-        "name" : "Hotel Emperador",
-        "stars" : "3",
-        "price" : "1596"
-      },
-      {
-        "name" : "Petit Palace San Bernardo",
-        "stars" : "4",
-        "price" : "2145"
-      },
-      {
-        "name" : "Hotel Nuevo Boston",
-        "stars" : "2",
-        "price" : "861"
-      }
-    ]
-  };
+  var hotels                    = require('./../mocks/hotels.json');
   res.status(200).json(hotels);
   
 };
